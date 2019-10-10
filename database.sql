a5cb4d14ad01acc15198f6b816b2d1891e21d408
himmatsinghwe <himmatsinghwe@users.noreply.github.com>
2019-10-10 17:41:39 +0530

    database
    database

---
+++ database.sql
@@ -0,0 +1,560 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.18 
+MySQL - 5.7.26-log : Database - benefit
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`benefit` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `benefit`;
+
+/*Table structure for table `admin_module_rights` */
+
+DROP TABLE IF EXISTS `admin_module_rights`;
+
+CREATE TABLE `admin_module_rights` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `admin_id` int(11) DEFAULT NULL,
+  `module` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `right_view` int(11) DEFAULT NULL COMMENT '1-yes, 0-no',
+  `right_add` int(11) DEFAULT NULL,
+  `right_edit` int(11) DEFAULT NULL,
+  `right_delete` int(11) DEFAULT NULL,
+  `right_others` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=212 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `admins` */
+
+DROP TABLE IF EXISTS `admins`;
+
+CREATE TABLE `admins` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) DEFAULT NULL,
+  `first_name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `last_name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `username` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `mobile` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` enum('active','inactive','deleted') COLLATE utf8_unicode_ci DEFAULT NULL,
+  `secret_key` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `otp` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `last_login` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `last_login_ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created` bigint(20) DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `modified` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `email` (`email`),
+  KEY `password` (`password`),
+  KEY `status` (`status`)
+) ENGINE=InnoDB AUTO_INCREMENT=238 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `articles` */
+
+DROP TABLE IF EXISTS `articles`;
+
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `content` text COLLATE utf8_unicode_ci,
+  `status` enum('active','inactive') COLLATE utf8_unicode_ci DEFAULT 'active',
+  `created_by` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `audit_history_logs` */
+
+DROP TABLE IF EXISTS `audit_history_logs`;
+
+CREATE TABLE `audit_history_logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `table_name` varchar(255) DEFAULT NULL,
+  `column_name` varchar(255) DEFAULT NULL,
+  `column_id` int(255) DEFAULT NULL,
+  `table_data` longtext,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Table structure for table `blog_messages` */
+
+DROP TABLE IF EXISTS `blog_messages`;
+
+CREATE TABLE `blog_messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `blog_id` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(2) NOT NULL DEFAULT '1',
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Table structure for table `blogs` */
+
+DROP TABLE IF EXISTS `blogs`;
+
+CREATE TABLE `blogs` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `publish_date` date NOT NULL,
+  `display_order` int(11) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `summary` text,
+  `content` text NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `thumb_image` varchar(255) DEFAULT NULL,
+  `tags` text,
+  `views` int(11) unsigned NOT NULL,
+  `created` datetime NOT NULL,
+  `last_updated_by` int(11) unsigned NOT NULL,
+  `status` enum('active','inactive') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `cities` */
+
+DROP TABLE IF EXISTS `cities`;
+
+CREATE TABLE `cities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `state_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `status` enum('active','inactive','deleted') DEFAULT 'active',
+  `created` datetime DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2712 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `coin_history` */
+
+DROP TABLE IF EXISTS `coin_history`;
+
+CREATE TABLE `coin_history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `coins` int(11) DEFAULT NULL,
+  `type` enum('earn','used','admin') DEFAULT NULL,
+  `reference_id` int(11) DEFAULT NULL,
+  `reference_type` enum('voucher','other') DEFAULT 'other',
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=295 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `coins` */
+
+DROP TABLE IF EXISTS `coins`;
+
+CREATE TABLE `coins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `total_coins` bigint(20) DEFAULT NULL,
+  `total_used` bigint(20) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=91 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `countries` */
+
+DROP TABLE IF EXISTS `countries`;
+
+CREATE TABLE `countries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` enum('active','inactive','deleted') COLLATE utf8_unicode_ci DEFAULT 'active',
+  `created_by` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `daily_limits` */
+
+DROP TABLE IF EXISTS `daily_limits`;
+
+CREATE TABLE `daily_limits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `limit` int(111) NOT NULL,
+  `effective_date` date NOT NULL,
+  `end_date` date DEFAULT NULL,
+  `created_by` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `faqs` */
+
+DROP TABLE IF EXISTS `faqs`;
+
+CREATE TABLE `faqs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `answer` text COLLATE utf8_unicode_ci,
+  `status` enum('active','inactive') COLLATE utf8_unicode_ci DEFAULT 'active',
+  `created_by` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `display_order` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `notify_templates` */
+
+DROP TABLE IF EXISTS `notify_templates`;
+
+CREATE TABLE `notify_templates` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `notify_type` enum('email','notification','sms','voice_call') COLLATE utf8_unicode_ci DEFAULT NULL,
+  `category` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `slug` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `remarks` text COLLATE utf8_unicode_ci,
+  `subject` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `content` text COLLATE utf8_unicode_ci,
+  `status` enum('active','inactive') COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified_by` bigint(20) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `pages` */
+
+DROP TABLE IF EXISTS `pages`;
+
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `title` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Title of the dynamic pages of the website',
+  `slug` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Slug of the page',
+  `heading` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Page heading',
+  `description` text COLLATE utf8_unicode_ci COMMENT 'Page content',
+  `status` enum('active','inactive','deleted') COLLATE utf8_unicode_ci DEFAULT 'active' COMMENT 'Page status',
+  `created_by` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL COMMENT 'Page created date',
+  `modified_by` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL COMMENT 'Page modified date',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `push_notifications` */
+
+DROP TABLE IF EXISTS `push_notifications`;
+
+CREATE TABLE `push_notifications` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` enum('common') DEFAULT NULL,
+  `sender_id` int(11) DEFAULT NULL,
+  `receiver_id` int(11) DEFAULT NULL,
+  `message` text,
+  `read_status` enum('read','unread') DEFAULT NULL,
+  `status` enum('active','inactive') DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=73 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `role_permissions` */
+
+DROP TABLE IF EXISTS `role_permissions`;
+
+CREATE TABLE `role_permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) DEFAULT NULL,
+  `module` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `right_view` int(11) DEFAULT NULL,
+  `right_add` int(11) DEFAULT NULL,
+  `right_edit` int(11) DEFAULT NULL,
+  `right_delete` int(11) DEFAULT NULL,
+  `right_others` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=134 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `roles` */
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` enum('active','inactive','deleted') COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `is_system` enum('yes','no') COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `running_histories` */
+
+DROP TABLE IF EXISTS `running_histories`;
+
+CREATE TABLE `running_histories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `steps` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `runnings` */
+
+DROP TABLE IF EXISTS `runnings`;
+
+CREATE TABLE `runnings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `total_steps` bigint(20) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `settings` */
+
+DROP TABLE IF EXISTS `settings`;
+
+CREATE TABLE `settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `coins` int(11) DEFAULT NULL,
+  `coins_step` int(11) DEFAULT NULL,
+  `calories` int(11) DEFAULT NULL,
+  `calories_step` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `contact_no` int(20) NOT NULL,
+  `contact_email` varchar(100) NOT NULL,
+  `from_email` varchar(100) NOT NULL,
+  `newsletter_email` varchar(100) NOT NULL,
+  `smtp_server` varchar(100) NOT NULL,
+  `smtp_port` varchar(100) NOT NULL,
+  `smtp_username` varchar(100) NOT NULL,
+  `smtp_password` varchar(100) NOT NULL,
+  `sms_server_id` varchar(100) NOT NULL,
+  `sms_server_password` varchar(100) NOT NULL,
+  `goal` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `states` */
+
+DROP TABLE IF EXISTS `states`;
+
+CREATE TABLE `states` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `region_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `status` enum('active','inactive','deleted') DEFAULT 'active',
+  `created` datetime DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2554 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `user_devices` */
+
+DROP TABLE IF EXISTS `user_devices`;
+
+CREATE TABLE `user_devices` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `device_type` enum('android','ios') NOT NULL,
+  `device_id` varchar(255) NOT NULL DEFAULT '',
+  `device_token` varchar(255) NOT NULL DEFAULT '',
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=575 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `user_login_logs` */
+
+DROP TABLE IF EXISTS `user_login_logs`;
+
+CREATE TABLE `user_login_logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT '0',
+  `ip_address` varchar(255) DEFAULT '',
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=760 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `user_notifications` */
+
+DROP TABLE IF EXISTS `user_notifications`;
+
+CREATE TABLE `user_notifications` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` enum('common') DEFAULT NULL,
+  `sender_id` int(11) DEFAULT NULL,
+  `receiver_id` int(11) DEFAULT NULL,
+  `message` text,
+  `read_status` enum('read','unread','deleted') DEFAULT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=354 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `user_orders` */
+
+DROP TABLE IF EXISTS `user_orders`;
+
+CREATE TABLE `user_orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `reference_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `reference_type` enum('other','voucher') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=95 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `user_otps` */
+
+DROP TABLE IF EXISTS `user_otps`;
+
+CREATE TABLE `user_otps` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `otp` varchar(20) DEFAULT '',
+  `end_date` datetime DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `mobile` varchar(15) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=60 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `user_passwords` */
+
+DROP TABLE IF EXISTS `user_passwords`;
+
+CREATE TABLE `user_passwords` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `user_settings` */
+
+DROP TABLE IF EXISTS `user_settings`;
+
+CREATE TABLE `user_settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `step_goal` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Table structure for table `user_steps` */
+
+DROP TABLE IF EXISTS `user_steps`;
+
+CREATE TABLE `user_steps` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `steps` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=698 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `lat` varchar(256) NOT NULL,
+  `long` varchar(256) NOT NULL,
+  `city` varchar(256) NOT NULL,
+  `dob` date NOT NULL,
+  `user_role_type` enum('admin','subadmin','user') DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `email_verified` enum('yes','no') DEFAULT 'no',
+  `mobile` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `finger_print` text,
+  `fb_token` varchar(255) DEFAULT NULL,
+  `secret_key` varchar(255) DEFAULT NULL,
+  `g_token` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `email_notifications` enum('yes','no') DEFAULT 'yes',
+  `push_notifications` enum('yes','no') DEFAULT 'yes',
+  `last_login` date DEFAULT NULL,
+  `last_login_ip` varchar(255) DEFAULT NULL,
+  `otp` varchar(255) DEFAULT NULL,
+  `status` enum('active','inactive','deleted','blocked') NOT NULL DEFAULT 'active',
+  `created_by` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=147 DEFAULT CHARSET=latin1 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
+
+/*Table structure for table `vouchers` */
+
+DROP TABLE IF EXISTS `vouchers`;
+
+CREATE TABLE `vouchers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `coins_required` int(11) DEFAULT NULL,
+  `discount_type` enum('fixed_amount','percentage') DEFAULT NULL,
+  `amount` decimal(10,0) DEFAULT NULL,
+  `max_discount` decimal(10,0) DEFAULT NULL,
+  `min_purchase` decimal(10,0) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `end_date` datetime DEFAULT NULL,
+  `status` enum('active','deleted','expired','inactive') DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified_by` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `descriptions` longtext,
+  `terms_and_conditions` longtext,
+  `bg_image` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=82 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
